d7cf182b6ab6f0ff5ab0c9648fcd7e8f3fa2c869
thisisshivamsingh <thisisshivam06@gmail.com>
2023-05-18 16:18:56 +0530

    Response structure changed

--- controllers/dropDownController.js
+++ controllers/dropDownController.js
@@ -4,7 +4,7 @@ exports.createDropDown = async (req, res, next) => {
   try {
     const doc = await DropDown.create(req.body);
     res.status(201).json({
-      status: "success",
+      message: "success",
       data: doc,
     });
   } catch (err) {
@@ -29,9 +29,7 @@ exports.getDropDown = async (req, res, next) => {
     const doc = await query.skip(skip).limit(limit);
     res.status(200).json({
       message: "success",
-      data: {
-        data: doc,
-      },
+      data: doc,
     });
   } catch (err) {
     res.status(404).json({
